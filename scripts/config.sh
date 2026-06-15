91cd5bf6565576d6f68221bfc84e03b33c41e129
zhang3800 <3638145593@qq.com>
2026-06-15 21:34:44 +0800

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,3 +6,10 @@ export HARBOR="192.168.10.14/solo"
 export IMAGE_REPO="${HARBOR}/dev/${APP_NAME}"
 
 export MVN_CMD="mvn clean install -Dmaven.test.skip=true -U"
+
+#定义k8s创建
+export DEPLOY_NAMESPACE="solo-dev"
+export DEPLOY_NAME="solo"
+export CONTAINER_NAME="solo"
+export ENABLE_SERVICE="true"
+export SERVICE_TYPE="NodePort"
