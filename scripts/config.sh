8d14cd84b0d17c3353ff7ff01cd8a9c43f47d4d9
zhang3800 <3638145593@qq.com>
2026-06-15 21:09:40 +0800

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,7 @@
 
 export APP_NAME="soloapp"
 export PROJECT="solo"
-export HARBOR="harbor.zrh.com/solo"
+export HARBOR="192.168.10.14/solo"
 export IMAGE_REPO="${HARBOR}/dev/${APP_NAME}"
 
 export MVN_CMD="mvn clean install -Dmaven.test.skip=true -U"
